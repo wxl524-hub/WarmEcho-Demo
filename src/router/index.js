afc68ce0e2425c5c8f3436b2ac2278708b92591f
wxl524-hub <wanxialin524@gmail.com>
2026-07-07 12:28:54 +0800

    fix: use hash history for GitHub Pages static hosting

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import WelcomePage from '../views/WelcomePage.vue'
 import ChatPage from '../views/ChatPage.vue'
 import CalmPage from '../views/CalmPage.vue'
@@ -24,7 +24,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes
 })
 
